d61f8658e99897e5442d609ad1a479c5fd6c2110
Amalay <84455469+amalay@users.noreply.github.com>
2021-10-13 13:01:11 +0530

    Updated

---
+++ OopsConcept/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Amalay.OopsConcept
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SolidPrinciple/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Amalay.SolidPrinciple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
