6c356eb7082b16a07b0f35702529232c2e5f0476
Amalay <84455469+amalay@users.noreply.github.com>
2021-10-14 15:10:49 +0530

    Updated

---
+++ SolidPrinciple/I1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amalay.SolidPrinciple.I1
+{
+    public interface ITask
+    {  
+        void CreateTask();  
+
+        void AssignTask();  
+
+        void Development();
+    }
+
+    public class TeamLead : ITask
+    {
+        public void CreateTask()
+        {
+            //Code to create a task            
+        }
+
+        public void AssignTask()
+        {
+            //Code to assign a task.  
+        }        
+
+        public void Development()
+        {
+            //Code to work on assigned task.  
+        }
+    }
+
+    public class Manager : ITask
+    {
+        public void CreateTask()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AssignTask()
+        {
+            //Code to assign a task.  
+        }
+
+        public void Development()
+        {
+            throw new Exception("Manager can't work on Task");
+        }
+    }
+}
---
+++ SolidPrinciple/I2.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amalay.SolidPrinciple.I2
+{
+    public interface ITask
+    {
+        void CreateTask();
+
+        void AssignTask();
+    }
+
+    public interface IDevelopment
+    {
+        void Development();
+    }
+
+    public class Developer : IDevelopment
+    {
+        public void Development()
+        {
+            //Code to work on assigned task. 
+        }
+    }
+
+    public class Manager : ITask
+    {
+        public void CreateTask()
+        {
+            //Code to create a task.  
+        }
+
+        public void AssignTask()
+        {
+            //Code to assign a Task  
+        }        
+    }
+
+    public class TeamLead : IDevelopment, ITask
+    {
+        public void CreateTask()
+        {
+            //Code to create a task.  
+        }
+
+        public void AssignTask()
+        {
+            //Code to assign a Task  
+        }
+
+        public void Development()
+        {
+            //Code to work on assigned task. 
+        }
+    }
+}
